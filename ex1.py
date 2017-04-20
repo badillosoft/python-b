504056e4666084c47553b9ba8586b173584c477d
Alan Badillo <badillo.soft@hotmail.com>
2017-04-19 19:21:07 -0500

    Sesión 14

---
+++ ex1.py
@@ -0,0 +1,49 @@
+### VARIABLES
+
+## Enteros
+
+# Crear una variable de tipo entero leida
+# desde el teclado sumarle 18 e imprimirla
+
+## Flotantes
+
+# Crear una variable de tipo flotante leida
+# desde el teclado, dividirla entre 18 e imprimirla
+
+## Booleanos
+
+# Crear una variable que contenga True 
+# si el usuario ingresa desde el teclado "SI"
+# o que contenga False en otro caso
+
+### COLECCIONES
+
+## Listas
+
+# Crear una lista con 5 elementos cualesquiera
+
+# Crear una lista con 1, 3, 5, 7, 9, ... 101
+
+# Crear una lista generada por
+# 2 * i - 1 for i in range(1, 51)
+
+## Tuplas
+
+# Crear una tupla con 4 elementos cualesquiera
+# y desempaquetar la tupla en las variables
+# a, b, x, y
+
+# Crear dos variables a, b que contengan
+# numeros diferentes y asignar el valor
+# de `a` a `b` y el valor de de `b` a `a`
+# al mismo tiempo usando una tupla y el
+# desempaquetamiento: a, b = (?, ?)
+
+## Diccionarios
+
+# Crear un diccionario que contenga 3 claves
+# cualesquiera con 3 valores cualesquiera
+
+# Crear un diccionario con las claves "x" y "y"
+# la clave "x" debe contener el sin(0.78) y
+# la clave "y" debe contener el cos(0.78)
